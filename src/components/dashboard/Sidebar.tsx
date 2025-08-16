a7288a12033f3b316c3785bd668c89760b123f33
RodCode <rodolfoantoniorq@gmail.com>
2025-08-16 15:21:02 +0200

    UpdatePlan

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -6,6 +6,7 @@ import { LuLogOut } from 'react-icons/lu';
 import { FaBars, FaTimes, FaSearch, FaDatabase, FaChartBar, FaCreditCard, FaCog } from 'react-icons/fa';
 import { MdDashboard } from 'react-icons/md';
 import { useAuth } from '../../context/AuthContext';  
+import { useRouter } from 'next/navigation';
 
 interface SidebarProps {
   isSidebarOpen: boolean;
@@ -13,12 +14,30 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setIsSidebarOpen }) => {
-  const { logout } = useAuth(); // Usamos el logout del AuthContext
+  const { logout, setUpdatePlanMode, setSearchMode } = useAuth(); // Usamos el logout del AuthContext
+  const router = useRouter();
 
   const handleLogout = () => {
     logout(); // Llama a la función de logout de tu contexto
     // Podrías añadir una redirección a la página de login aquí si tu contexto no la maneja
     console.log("Sesión cerrada desde el sidebar.");
+  };
+
+    // Maneja la navegación a una ruta específica
+  // const navigateTo = (path: string) => {
+  //   router.push(path);
+  //   setIsSidebarOpen(false);
+  // };
+
+  const handleSubscriptionClick = () => {
+    setUpdatePlanMode(true); // Establece updatePlanMode en true
+    setSearchMode(false); // Establece setSearchMode en false
+    setIsSidebarOpen(false); // Cierra la barra lateral
+  };
+  const handleDashboardClick = () => {
+    setUpdatePlanMode(false); // Establece updatePlanMode en true
+    setSearchMode(false); // Establece setSearchMode en false
+    setIsSidebarOpen(false); // Cierra la barra lateral
   };
 
   return (
@@ -46,7 +65,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setIsSidebarOpen }) =>
             <ul>
               <li className="mb-4">
                 <a href="#" className="flex items-center text-gray-700 hover:text-blue-600 font-semibold"
-                   onClick={() => setIsSidebarOpen(false)}>
+                   onClick={handleDashboardClick}>
                   <MdDashboard className="text-lg" />
                   <span className="ml-2">Dashboard</span>
                 </a>
@@ -81,7 +100,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setIsSidebarOpen }) =>
           <ul>
             <li className="mb-4">
               <a href="#" className="flex items-center text-gray-700 hover:text-blue-600"
-                 onClick={() => setIsSidebarOpen(false)}>
+                 //</li>onClick={() => setIsSidebarOpen(false)}>
+                 onClick={handleSubscriptionClick}>
                 <FaCreditCard className="text-lg" />
                 <span className="ml-2">Suscripción</span>
               </a>
@@ -95,6 +115,27 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setIsSidebarOpen }) =>
             </li>
           </ul>
         </div>
+
+         {/* <div>
+          <h3 className="text-gray-500 uppercase text-xs font-semibold mb-4">Cuenta</h3>
+          <ul>
+            <li className="mb-4">
+              <a href="#" className="flex items-center text-gray-700 hover:text-blue-600"
+                 onClick={() => navigateTo('/dashboard/suscripcion')}>
+                <svg className="text-lg w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 10h18M7 15h1m4 0h1m4 0h1M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
+                <span className="ml-2">Suscripción</span>
+              </a>
+            </li>
+            <li className="mb-4">
+              <a href="#" className="flex items-center text-gray-700 hover:text-blue-600"
+                 onClick={() => navigateTo('/dashboard/settings')}>
+                <svg className="text-lg w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37a1.724 1.724 0 002.572-1.065z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
+                <span className="ml-2">Configuración</span>
+              </a>
+            </li>
+          </ul>
+        </div> */}
+
         {/* Botón de Cerrar Sesión en el sidebar */}
         <button
           onClick={handleLogout}
